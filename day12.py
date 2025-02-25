d8fdca11b678b6346b1a9ca0b9e5ab08ae88bac8
ChoiMinSang <min393917@gmail.com>
2025-02-25 16:39:28 +0900

    v0.9 day12 ml assignment

--- day12.py
+++ day12.py
@@ -1,14 +1,17 @@
-#Assignment
-#v0.9) v0.8 파일의 결측치 값을 산술평균으로 채워 넣는 다양한 방법을 적용하시오.
+# Assignment
+# v0.9) v0.8 파일의 결측치 값을 산술평균으로 채워 넣는 다양한 방법을 적용하시오.
 import numpy as np
 import pandas as pd
 from sklearn.impute import SimpleImputer
 
 df = pd.DataFrame(
     {
-        'A':[1,2,np.nan,4],
-        'B':[np.nan,2,3,4],
-        'C':[1,2,3,4]
+        'A':[1, 2, np.nan, 4],
+        'B':[np.nan, 12, 3, 4],
+        'C':[1, 2, 3, 4]
     }
 )
+print(df)
+i = SimpleImputer(strategy='mean')
+df[['A', 'B']] = i.fit_transform(df[['A', 'B']])
 print(df)
